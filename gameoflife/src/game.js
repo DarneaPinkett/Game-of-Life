20094bac943bf72f492c3d863835b5ae2d1d7599
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-07-23 18:50:45 -0400

    React app created

---
+++ gameoflife/src/game.js
@@ -0,0 +1,11 @@
+import React from 'react'; import './game.css';
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {render() {
+    return ()
+}}
+
+export default Game;
