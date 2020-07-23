59769ed024cbc947e762bbb05c410351c978bc66
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-07-23 19:08:09 -0400

    Game comp

--- gameoflife/src/game.js
+++ gameoflife/src/game.js
@@ -1,13 +1,17 @@
-import React from 'react'; import './game.css';
+import React from 'react'; 
+import './game.css';
 
 const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
-class Game extends React.Component {render() {
-    return (
-        <div className="Board" style{{ width: WIDTH, height: HEIGHT}}
-    )
-}}
+class Game extends React.Component{
+    render() {
+        return(
+            <div className="Board"
+            style={{ width: WIDTH, height: HEIGHT}}></div>
+        )
+    }
+}
 
-export default Game;
+export default Game
